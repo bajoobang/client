675178ec73bf5815e55b34769ce08c2a2f845ea8
youmeanit <umin030312@naver.com>
2024-08-23 11:24:20 +0900

    🐛

--- BaJooBang/src/main/frontend/src/pages/MyPage/Mypage.js
+++ BaJooBang/src/main/frontend/src/pages/MyPage/Mypage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './MyPage.css'; 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 import { ReactComponent as Profile } from '../../components/images/mp_profile.svg';
 
@@ -17,13 +17,13 @@ function MyPage() {
 
   useEffect(() => {
     const fetchData = async () => {
-        try {
-            const response = await axios.get('/member'); // Replace with your actual API endpoint
-            const requestData = response.data;
-            setListData(requestData);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+      try {
+        const response = await axios.get('/member'); // Replace with your actual API endpoint
+        const requestData = response.data;
+        setListData(requestData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
     fetchData();
   }, []);
@@ -46,42 +46,41 @@ function MyPage() {
           
           <div className='ActivitySection'>
             <div className='RequestorActivity'>
-              <h3>나의 요청인 활동</h3><p></p>
+              <h3>나의 요청인 활동</h3>
               <div className='ActivityBox'>
-                <div className='ActivityItem' Link to>
+                <NavLink className='ActivityItem' to='/member/heart'>
                   <p>찜한 방</p>
                   <p>{listData.numOfLikes}개</p>
-                </div>
-                <div className='ActivityItem'>
+                </NavLink>
+                <NavLink className='ActivityItem' to='/member/registered'>
                   <p>등록 매물</p>
                   <p>{listData.numOfRegistered}건</p>
-                </div>
-                <div className='ActivityItem'>
+                </NavLink>
+                <NavLink className='ActivityItem' to='/member/inquiry'>
                   <p>신청 조회</p>
                   <p>{listData.numOfInquiries}건</p>
-                </div>
+                </NavLink>
               </div>
-              <Link to='/member/registered' className='ViewMapLink'>매물 지도 보러가기</Link>
+              <NavLink to='/member/registered' className='ViewMapLink'>매물 지도 보러가기</NavLink>
             </div>
 
             <div className='FootworkActivity'>
-              <p></p>
-              <h3>나의 발품인 활동</h3><p></p>
+              <h3>나의 발품인 활동</h3>
               <div className='ActivityBox'>
                 <div className='ActivityItem'>
                   <p>나의 별점</p>
                   <p>{listData.star}</p>
                 </div>
-                <div className='ActivityItem'>
+                <NavLink className='ActivityItem' to='/member/footwork'>
                   <p>신청 발품</p>
                   <p>{listData.numOfFootworks}건</p>
-                </div>
-                <div className='ActivityItem'>
+                </NavLink>
+                <NavLink className='ActivityItem' to='/member/alarm'>
                   <p>알림</p>
                   <p>{listData.numOfAlarms}건</p>
-                </div>
+                </NavLink>
               </div>
-              <Link to='/member/footwork' className='ViewMapLink'>발품 지도 보러가기</Link>
+              <NavLink to='/member/footwork' className='ViewMapLink'>발품 지도 보러가기</NavLink>
             </div>
           </div>
         </div>
